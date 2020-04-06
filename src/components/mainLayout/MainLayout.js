ba67b74063d6257945cd40f24e1a2b7a8967c478
halilcodex <ibrahim_amoo@yahoo.com>
2020-04-06 17:44:35 +0100

    SECOND initialization done

--- src/components/mainLayout/MainLayout.js
+++ src/components/mainLayout/MainLayout.js
@@ -1,12 +1,28 @@
 import React from "react";
-import Alert from "react-bootstrap/Alert";
+// import Alert from "react-bootstrap/Alert";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function MainLayout() {
+import ngmLogo from "../../assets/NGM-logo.png";
+import "./mainlayout.css";
+
+function MainLayout(props) {
   return (
-    <div>
-      <Alert variant="success">
-        <p>You are all doing well</p>
-      </Alert>
+    <div className="mainlayout">
+      <Navbar className="my-navbar" bg="light">
+        <Navbar.Brand>
+          <img className="logo" src={ngmLogo} alt="NGM" />
+        </Navbar.Brand>
+        <Nav className="justify-content-end">
+          <Nav.Item>
+            <Nav.Link>Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Link>Pages</Nav.Link>
+          <Nav.Link>Portfolio</Nav.Link>
+          <Nav.Link>Blog</Nav.Link>
+        </Nav>
+      </Navbar>
+      {props.children}
     </div>
   );
 }
