b8579734d66379a4eb9ddc10de0c11559fb514d3
Bhaelar <okenlabasit@gmail.com>
2020-04-24 14:00:12 +0100

    Implementing displaying tooltip from array

--- src/pages/members/Members.js
+++ src/pages/members/Members.js
@@ -1,17 +1,27 @@
 import React, { useState, useRef } from 'react';
-import Popover from 'react-bootstrap/Popover';
-import Overlay from 'react-bootstrap/Overlay';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import './Members.css';
 
 class Members extends React.Component {
-	renderTooltip = function (props) {
+	state = {
+		value: ''
+	}
+	constructor(props) {
+		super(props)
+		this.bar = this.bar.bind(this);
+	}
+	
+	bar(e){
+		alert(e.currentTarget.id);
+		this.setState({value: e.currentTarget.id})
+	}
+
+	renderTooltip = function (props, imageId) {
+		const wordsArray = ["First post", "second post", "Third post"]
 		return (
 			<Tooltip id="button-tooltip" {...props}>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime blanditiis aliquid atque praesentium,
-				laudantium exercitationem fuga ex consectetur? Iste tempora nemo tempore eum magni animi sequi quam
-				neque architecto facilis!
+				{wordsArray[0]}
 			</Tooltip>
 		);
 	};
@@ -43,38 +53,40 @@ class Members extends React.Component {
 
 					<div class="container">
 						<div class="row">
-							<div class="member-row col-lg-4 col-md-4 col-sm-4 col-xs-6">
+							<div class="member-row col-lg-4 col-md-6 col-sm-12" id="0" onClick={this.bar}>
 								<OverlayTrigger
+									id="0"
 									placement="bottom"
 									delay={{ show: 250, hide: 400 }}
 									overlay={this.renderTooltip}
 								>
-									<img src="http://fakeimg.pl/250x250/" class="img-responsive member-img" />
+									<img src="http://fakeimg.pl/250x250/" class="img-responsive member-img" id="0" />
 								</OverlayTrigger>
 
 								<h3>First Member</h3>
 								<p>Cras justo odio, dapibus ac facilisis in</p>
 							</div>
 
-							<div class="member-row col-lg-4 col-md-4 col-sm-4 col-xs-6">
+							<div class="member-row col-lg-4 col-md-6 col-sm-12">
 								<OverlayTrigger
+									id="1"
 									placement="bottom"
 									delay={{ show: 250, hide: 400 }}
 									overlay={this.renderTooltip}
 								>
-									<img src="http://fakeimg.pl/250x250/" class="img-responsive member-img" />
+									<img src="http://fakeimg.pl/250x250/" class="img-responsive member-img" id="1" />
 								</OverlayTrigger>
 								<h3>Second Member</h3>
 								<p>Cras justo odio, dapibus ac facilisis in</p>
 							</div>
 
-							<div class="member-row col-lg-4 col-md-4 col-sm-4 col-xs-6">
+							<div class="member-row col-lg-4 col-md-6 col-sm-12">
 								<OverlayTrigger
 									placement="bottom"
 									delay={{ show: 250, hide: 400 }}
 									overlay={this.renderTooltip}
 								>
-									<img src="http://fakeimg.pl/250x250/" class="img-responsive member-img" />
+									<img src="http://fakeimg.pl/250x250/" class="img-responsive member-img"  id="2"/>
 								</OverlayTrigger>
 								<h3>Third Member</h3>
 								<p>Cras justo odio, dapibus ac facilisis in</p>
