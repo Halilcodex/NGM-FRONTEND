34f177f20007ad562135d78dcfea9bc7e700f854
halilcodex <ibrahim_amoo@yahoo.com>
2020-04-25 01:17:28 +0100

    made some fixes to gallery page

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,103 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gallery.css";
+import Carousel from "react-bootstrap/Carousel";
+import "../home/Home.css";
+import pic from "../../assets/pic1.jpg";
+import pic2 from "../../assets/pic2.jpg";
+import pic3 from "../../assets/pic3.jpg";
 
 function Gallery() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <div className="gallery-container">
-      <div class="cover-container d-flex w-100 h-100 mx-auto flex-column">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <svg
-                class="bd-placeholder-img"
-                width="100%"
-                height="100%"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                role="img"
-              >
-                <rect width="100%" height="100%" fill="#777" />
-              </svg>
-              <div class="container">
-                <div class="carousel-caption">
-                  <p>
-                    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                    Donec id elit non mi porta gravida at eget metus. Nullam id
-                    dolor id nibh ultricies vehicula ut id elit.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <svg
-                class="bd-placeholder-img"
-                width="100%"
-                height="100%"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                role="img"
-              >
-                <rect width="100%" height="100%" fill="#777" />
-              </svg>
-              <div class="container">
-                <div class="carousel-caption">
-                  <p>
-                    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                    Donec id elit non mi porta gravida at eget metus. Nullam id
-                    dolor id nibh ultricies vehicula ut id elit.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <svg
-                class="bd-placeholder-img"
-                width="100%"
-                height="100%"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                role="img"
-              >
-                <rect width="100%" height="100%" fill="#777" />
-              </svg>
-              <div class="container">
-                <div class="carousel-caption">
-                  <p>
-                    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                    Donec id elit non mi porta gravida at eget metus. Nullam id
-                    dolor id nibh ultricies vehicula ut id elit.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <a
-            class="carousel-control-prev"
-            href="#myCarousel"
-            role="button"
-            data-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a
-            class="carousel-control-next"
-            href="#myCarousel"
-            role="button"
-            data-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
+    <div className="gallery-container home-container">
+      <div class="carousel-container">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <div
+              className="img-div"
+              style={{ backgroundImage: `url(${pic})` }}
+            ></div>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div
+              className="img-div"
+              style={{ backgroundImage: `url(${pic2})` }}
+            ></div>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div
+              className="img-div"
+              style={{ backgroundImage: `url(${pic3})` }}
+            ></div>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     </div>
   );
