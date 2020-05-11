53933b700d1de8e153ba2b58e8a3c0d166b4ff14
halilcodex <ibrahim_amoo@yahoo.com>
2020-05-11 01:55:28 +0100

    remodified the gallery's page based on the NGM suggestion

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,65 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Gallery.css";
-import Carousel from "react-bootstrap/Carousel";
-import "../home/Home.css";
 import pic from "../../assets/pic1.jpg";
 import pic2 from "../../assets/pic2.jpg";
 import pic3 from "../../assets/pic3.jpg";
-import Icon from "react-icons-kit";
-import { chevronCircleRight } from "react-icons-kit/fa/chevronCircleRight";
-import { chevronCircleLeft } from "react-icons-kit/fa/chevronCircleLeft";
+import story from "../../assets/story.jpg";
+import unsplash2 from "../../assets/unsplash2.jpg";
 
 function Gallery() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-  const nextIcon = <Icon icon={chevronCircleRight} size={40} />;
-  const prevIcon = <Icon icon={chevronCircleLeft} size={40} />;
   return (
-    <div className="gallery-container home-container">
-      <div class="carousel-container">
-        <Carousel
-          nextIcon={nextIcon}
-          prevIcon={prevIcon}
-          activeIndex={index}
-          onSelect={handleSelect}
+    <div className="gallery-container">
+      <section className="story-in-images">
+        <div
+          className="story-image"
+          style={{ backgroundImage: `url(${story})` }}
         >
-          <Carousel.Item>
-            <div
-              className="img-div"
-              style={{ backgroundImage: `url(${pic})` }}
-            ></div>
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div
-              className="img-div"
-              style={{ backgroundImage: `url(${pic2})` }}
-            ></div>
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div
-              className="img-div"
-              style={{ backgroundImage: `url(${pic3})` }}
-            ></div>
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
+          <h1>Our Story in Images</h1>
+        </div>
+      </section>
+      <section className="stories">
+        <img src={pic} alt="assets" />
+        <img src={pic2} alt="assets" />
+        <img src={pic3} alt="assets" />
+        <img src={story} alt="assets" />
+        <iframe
+          width="400"
+          height="315"
+          src="https://www.youtube.com/embed/ZCFkWDdmXG8"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        {/* <img src={unsplash2} alt="assets" /> */}
+        <img src={story} alt="assets" />
+      </section>
     </div>
   );
 }
